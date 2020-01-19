d0c819b8dc7b807aee1b34aa752c6a25fafb39fa
Miguel Sanchez Gonzalez <miguelsanychez@gmail.com>
2020-01-19 19:00:49 +0100

    chore: rename files

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const rpl = require( './src/rpl.js' );
+const cli = require( './src/cli.js' );
 const search = require( './src/search.js' );
 
 module.exports = ( candidates, prompt, renderLine, options ) =>
-    rpl( search( candidates, options ), prompt, renderLine );
+    cli( search( candidates, options ), prompt, renderLine );
